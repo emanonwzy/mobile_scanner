37ba489ab1012657cbea6fc871897f39421e2c63
wuzy <emanonwzy@gmail.com>
2022-11-23 20:42:06 +0800

    fetch douban isbn

--- example/lib/barcode_scanner_returning_image.dart
+++ example/lib/barcode_scanner_returning_image.dart
@@ -2,6 +2,8 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:mobile_scanner_example/barcode_fetcher.dart';
+import 'package:html/parser.dart' show parse;
 
 class BarcodeScannerReturningImage extends StatefulWidget {
   const BarcodeScannerReturningImage({Key? key}) : super(key: key);
@@ -25,6 +27,7 @@ class _BarcodeScannerReturningImageState
   );
 
   bool isStarted = true;
+  String isbnContent = "";
 
   @override
   Widget build(BuildContext context) {
@@ -38,23 +41,28 @@ class _BarcodeScannerReturningImageState
                 color: Colors.blueGrey,
                 width: double.infinity,
                 height: 0.33 * MediaQuery.of(context).size.height,
-                child: barcode?.image != null
-                    ? Transform.rotate(
-                        angle: 90 * pi / 180,
-                        child: Image(
-                          gaplessPlayback: true,
-                          image: MemoryImage(barcode!.image!),
-                          fit: BoxFit.contain,
-                        ),
-                      )
-                    : const ColoredBox(
-                        color: Colors.white,
-                        child: Center(
-                          child: Text(
-                            'Your scanned barcode will appear here!',
-                          ),
-                        ),
-                      ),
+                // child: barcode?.image != null
+                //     ? Transform.rotate(
+                //         angle: 90 * pi / 180,
+                //         child: Image(
+                //           gaplessPlayback: true,
+                //           image: MemoryImage(barcode!.image!),
+                //           fit: BoxFit.contain,
+                //         ),
+                //       )
+                //     : const ColoredBox(
+                //         color: Colors.white,
+                //         child: Center(
+                //           child: Text(
+                //             'Your scanned barcode will appear here!',
+                //           ),
+                //         ),
+                //       ),
+                child: Scrollbar(
+                  child: SingleChildScrollView(
+                    child: Text(isbnContent),
+                  ),
+                ),
               ),
               Container(
                 height: 0.66 * MediaQuery.of(context).size.height,
@@ -69,11 +77,35 @@ class _BarcodeScannerReturningImageState
                       //   torchEnabled: true,
                       //   facing: CameraFacing.front,
                       // ),
-                      onDetect: (barcode, arguments) {
-                        setState(() {
-                          this.arguments = arguments;
-                          this.barcode = barcode;
-                        });
+                      onDetect: (barcode, arguments) async {
+                        this.arguments = arguments;
+                        this.barcode = barcode;
+
+                        if (this.barcode?.barcodes.first.rawValue != null) {
+                          final _isbnContent = await fetchIsbn(
+                            (this.barcode?.barcodes.first.rawValue)!,
+                          );
+                          var document = parse(_isbnContent);
+                          final element = document
+                              .getElementsByClassName("result")[0]
+                              .children[1];
+
+                          final title = element.children[0];
+                          final abstractContent = element.children[1].innerHtml;
+
+                          final buffer = StringBuffer();
+                          buffer.write(title);
+                          // buffer.write(element.outerHtml);
+                          // for (final element in element.children) {
+                          //   buffer.write(element.className);
+                          //   buffer.write("\n");
+                          //   buffer.write(element.attributes);
+                          //   buffer.write("\n\n");
+                          // }
+                          setState(() {
+                            isbnContent = buffer.toString();
+                          });
+                        }
                       },
                     ),
                     Align(
